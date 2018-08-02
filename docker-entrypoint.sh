2600abb19d57d33c69040790df55675a981df461
Aaron Diep <aaron@delite.ca>
2018-08-01 21:51:41 -0600

    clear entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-if [ "$NODE_ENV" == "test" ] && [ ! -f "node_modules/.bin/mocha" ]; then
-    DEV_PACKAGES="make gcc g++ python git"
-    apk add --no-cache $DEV_PACKAGES
-    npm install
-    apk del $DEV_PACKAGES
-fi
-
 exec "$@"
