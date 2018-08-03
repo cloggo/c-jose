c4b2780102606ad3f09718cae2e47ba44591a9d6
Aaron Diep <aaron@delite.ca>
2018-08-02 23:06:33 -0600

    jose header file

--- deps/patch.sh
+++ deps/patch.sh
@@ -3,6 +3,9 @@
 dup_names="misc.c jwk.c"
 src_dir=jose/lib/openssl
 
+cp jose/jose/jose.h.in jose/jose/jose.h
+sed -i -e 's/@VERSION@/10/g' jose/jose/jose.h
+
 for name in $dup_names; do
     if [ -f "$src_dir/$name" ]; then
         mv -f $src_dir/$name $src_dir/_$name
