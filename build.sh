05e54546345e62a8cbb9c23b380d31b710aab3c0
Aaron Diep <aaron@delite.ca>
2018-08-02 20:32:40 -0600

    fixes duplicate

--- build.sh
+++ build.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
+dup_names="misc.c jwk.c"
+src_dir=deps/jose/lib/openssl
 
-if [ $(uname -s) == "Darwin" ]; then
-  echo "fixes duplicate"
-
-    dup_names="misc.c jwk.c"
-    src_dir=deps/jose/lib/openssl
-
-    for name in $dup_names; do
-        if [ -f "$src_dir/$name" ]; then
-            mv $src_dir/$name $src_dir/_$name
-        fi
-    done
-fi
+for name in $dup_names; do
+    if [ -f "$src_dir/$name" ]; then
+        mv $src_dir/$name $src_dir/_$name
+    fi
+done
 
 rm -rf ./build
 node-gyp configure -- --no-duplicate-basename-check
